68797bbcb49b6735b9b6b609e66081b3b81e7e92
AnikaZN <anikanacey@gmail.com>
2019-10-08 21:13:36 -0600

    Upload TwitOff project code

---
+++ twitoff/app.py
@@ -0,0 +1,50 @@
+from decouple import config
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from .models import DB, User
+from .twitter import add_or_update_user
+from .predict import predict_user
+
+load_dotenv()
+
+def create_app():
+    """Create and configure an instance of the Flask application"""
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    DB.init_app(app)
+
+    @app.route('/')
+    def root():
+        return render_template('base.html', title='TwitOff', users=User.query.all())
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.username == name).one().tweets
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets, message=message)
+
+    @app.route('/compare', methods = ['GET', 'POST'])
+    def comparisons(message = ""):
+        user1 = request.values['user1']
+        user2 = request.values['user2']
+        tweet_text = request.values['tweet_text']
+
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+
+        return render_template('prediction.html', message=message)
+
+    return app
